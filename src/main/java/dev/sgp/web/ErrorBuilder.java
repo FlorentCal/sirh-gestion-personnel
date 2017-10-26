0cc64e833ec70d836e20a37a5eec69a4c95c26da
Florent Callaou <florent.callaou22@gmail.com>
2017-10-26 14:28:43 +0200

    ErrorBuilder + PageBuilder

---
+++ src/main/java/dev/sgp/web/ErrorBuilder.java
@@ -0,0 +1,18 @@
+package dev.sgp.web;
+
+public class ErrorBuilder {
+	
+	public static String buildError(String... params){
+		String error = "";
+				
+		for (String param : params) {
+			String[] paramsTab = param.split(":");
+			if(paramsTab[1].trim().equals("null")){
+				error += paramsTab[0].trim() + ". ";
+			}
+		}
+		
+		return error;
+	}
+
+}
---
+++ src/main/java/dev/sgp/web/PageBuilder.java
@@ -0,0 +1,15 @@
+package dev.sgp.web;
+
+public class PageBuilder {
+
+	public static String buildPage(String... params){
+		String page= "<h1>Edition de collaborateur</h1>"+ "<ul>";
+		
+		for (String param : params) {
+			page += "<li>"+ param +"</li>";		
+		}
+		
+		page += "</ul>";
+		return page;
+	}
+}
